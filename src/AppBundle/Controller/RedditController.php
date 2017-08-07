ca8cf9e075efe7543ba106fccbb8804a15b5e175
prrzeemoo <przemekstopka@gmail.com>
2017-08-07 14:29:58 +0200

    READ \ UPDATE \ DELETE RECORDS FORM DATABASE

--- src/AppBundle/Controller/RedditController.php
+++ src/AppBundle/Controller/RedditController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\RedditPost;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -17,23 +18,84 @@ class RedditController extends Controller
     /**
      * @Route("/", name="list")
      */
-    public function listAction() {
-
+    public function listAction()
+    {
 //        WSZYSTKIE REKORDY Z TABELI
-//        $posts = $this->getDoctrine()->getRepository('AppBundle:RedditPost')->findAll();
+        $posts = $this->getDoctrine()->getRepository('AppBundle:RedditPost')->findAll();
 //        DWA PONIŻSZE PRZYKŁADY POSZCZEGÓLNE REKORDY Z TBELI
 //        $posts = $this->getDoctrine()->getRepository('AppBundle:RedditPost')->find(1);
 //        $posts = $this->getDoctrine()->getRepository('AppBundle:RedditPost')->findOneBy([
 //           'id' => [3]
 //        ]);
-        $posts = $this->getDoctrine()->getRepository('AppBundle:RedditPost')->findBy([
-           'id' => [1,2]
-        ]);
+//        $posts = $this->getDoctrine()->getRepository('AppBundle:RedditPost')->findBy([
+//           'id' => [1,2]
+//        ]);
 
-        dump($posts);
+//        dump($posts);
 
         return $this->render('reddit/index.html.twig', [
             'posts' => $posts
         ]);
+    }
+
+    /**
+     * @Route("/create/{text}", name="create")
+     */
+    public function createAction($text)
+    {
+        $en = $this->getDoctrine()->getManager();
+
+        $post = new RedditPost();
+        $post->setTitle('hello ' . $text);
+
+//        PERSIST WYMAGANY TYLKO W PRZYPAKU TWORZENIA NOWEGO OBIEKTU
+        $en->persist($post);
+        $en->flush();
+
+        return $this->redirectToRoute('list');
+    }
+
+    /**
+     * @Route("/update/{id}/{text}", name="update")
+     */
+    public function updateAction($id, $text)
+    {
+        $en = $this->getDoctrine()->getManager();
+
+        $post = $en->getRepository('AppBundle:RedditPost')->find($id);
+
+        if (!$post)
+        {
+            throw $this->createNotFoundException('thats not a record');
+        }
+
+        /**
+         * @var $post RedditPost
+         */
+        $post->setTitle('updated title ' . $text);
+
+        $en->flush();
+
+        return $this->redirectToRoute('list');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function deleteAction($id)
+    {
+        $en = $this->getDoctrine()->getManager();
+
+        $post = $en->getRepository('AppBundle:RedditPost')->find($id);
+
+        if (!$post)
+        {
+            return $this->redirectToRoute('list');
+        }
+
+        $en->remove($post);
+        $en->flush();
+
+        return $this->redirectToRoute('list');
     }
 }
